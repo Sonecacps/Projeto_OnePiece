9860383277086b20aa1e2ff196cd55d4738d40cd
Kelvin Magalhaes <117312545+Sonecacps@users.noreply.github.com>
2023-10-01 21:39:54 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () =>{
+        desselecionarBotao();
+		desselecionarPersonagem();
+// adiciona selecionado
+        botao.classList.add("selecionado");
+		personagens[indice].classList.add("selecionado");
+    });
+});
+
+//remove selecionado do personagem
+function desselecionarPersonagem() {
+	const personagemSelecionado = document.querySelector(".personagem.selecionado");
+	personagemSelecionado.classList.remove("selecionado");
+}
+
+// remove selecionado do botao
+function desselecionarBotao() {
+	const botaoSelecionado = document.querySelector(".botao.selecionado");
+	botaoSelecionado.classList.remove("selecionado");
+
+}
+
+/*const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+	botao.addEventListener("click", () => {
+		desselecionarBotao();
+		desselecionarPersonagem();
+
+		botao.classList.add("selecionado");
+		personagens[indice].classList.add("selecionado");
+	});
+});
+
+function desselecionarPersonagem() {
+	const personagemSelecionado = document.querySelector(".personagem.selecionado");
+	personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+	const botaoSelecionado = document.querySelector(".botao.selecionado");
+	botaoSelecionado.classList.remove("selecionado");
+}*/
